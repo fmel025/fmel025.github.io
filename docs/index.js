47218a10bb248d89df2e7ef6896d2bf5180f7f7f
Fernando Meléndez <00018720@uca.edu.sv>
2023-03-04 13:57:01 -0600

    🐛 Fixed url links

--- docs/index.js
+++ docs/index.js
@@ -4,13 +4,13 @@ const URL_BASE = "https://fmel025.github.io/";
 
 
 fetch(`https://api.github.com/repos/fmel025/fmel025.github.io/contents/docs`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const htmlFiles = [...data];
-    const links = htmlFiles.map(file => `<li class="list-group-item"><a href="${URL_BASE}/${file.name}">${file.name}</a></li>`);
-    const pageCount = Math.ceil(links.length / itemsPerPage);
-    const pagination = `
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const htmlFiles = data.filter(file => !(file.name.endsWith(".html") || file.name.endsWith(".js")));
+        const links = htmlFiles.map(file => `<li class="list-group-item"><a href="${URL_BASE}/${file.name}">${file.name}</a></li>`);
+        const pageCount = Math.ceil(links.length / itemsPerPage);
+        const pagination = `
       <nav>
         <ul class="pagination justify-content-center">
           ${Array.from({ length: pageCount }, (_, i) => `
@@ -21,20 +21,20 @@ fetch(`https://api.github.com/repos/fmel025/fmel025.github.io/contents/docs`)
         </ul>
       </nav>
     `;
-    document.getElementById('pagination').innerHTML = pagination;
-    document.querySelectorAll('.page-link').forEach(link => {
-      link.addEventListener('click', event => {
-        event.preventDefault();
-        const page = parseInt(link.getAttribute('data-page'));
-        const startIndex = (page - 1) * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
-        const pageLinks = links.slice(startIndex, endIndex);
-        const toc = `<ul class="list-group">${pageLinks.join('')}</ul>`;
-        document.getElementById('toc').innerHTML = toc;
-        document.querySelectorAll('.page-item').forEach(item => item.classList.remove('active'));
-        link.parentElement.classList.add('active');
-      });
-    });
-    document.querySelector('.page-link[data-page="1"]').click();
-  })
-  .catch(error => console.error(error));
+        document.getElementById('pagination').innerHTML = pagination;
+        document.querySelectorAll('.page-link').forEach(link => {
+            link.addEventListener('click', event => {
+                event.preventDefault();
+                const page = parseInt(link.getAttribute('data-page'));
+                const startIndex = (page - 1) * itemsPerPage;
+                const endIndex = startIndex + itemsPerPage;
+                const pageLinks = links.slice(startIndex, endIndex);
+                const toc = `<ul class="list-group">${pageLinks.join('')}</ul>`;
+                document.getElementById('toc').innerHTML = toc;
+                document.querySelectorAll('.page-item').forEach(item => item.classList.remove('active'));
+                link.parentElement.classList.add('active');
+            });
+        });
+        document.querySelector('.page-link[data-page="1"]').click();
+    })
+    .catch(error => console.error(error));
