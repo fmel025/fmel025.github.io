d54ecc5c2d51f681d030cb0020dfd17633d44fb5
Fernando Meléndez <00018720@uca.edu.sv>
2023-03-04 20:22:02 -0600

    🎨 Merge branch 'main' of https://github.com/fmel025/fmel025.github.io

--- docs/index.js
+++ docs/index.js
@@ -1,39 +1,110 @@
-const itemsPerPage = 5;
+// Getting the elements from the DOM
+const coursesList = document.getElementById('coursesList');
+const pagination = document.querySelector('.pagination');
 
-const URL_BASE = "https://fmel025.github.io/";
+const BASE_URL = 'https://fmel025.github.io/';
+const fetchURL = 'https://api.github.com/repos/fmel025/fmel025.github.io/contents/docs';
 
-fetch(`https://api.github.com/repos/fmel025/fmel025.github.io/contents/docs`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const htmlFiles = data.filter(file => !(file.name.endsWith(".html") || file.name.endsWith(".js")));
-        const links = htmlFiles.map(file => `<li class="list-group-item"><a href="${URL_BASE}/${file.name}">${file.name}</a></li>`);
-        const pageCount = Math.ceil(links.length / itemsPerPage);
-        const pagination = `
-      <nav>
-        <ul class="pagination justify-content-center">
-          ${Array.from({ length: pageCount }, (_, i) => `
-            <li class="page-item${i === 0 ? ' active' : ''}">
-              <a class="page-link" href="#" data-page="${i + 1}">${i + 1}</a>
-            </li>
-          `).join('')}
-        </ul>
-      </nav>
-    `;
-        document.getElementById('pagination').innerHTML = pagination;
-        document.querySelectorAll('.page-link').forEach(link => {
-            link.addEventListener('click', event => {
-                event.preventDefault();
-                const page = parseInt(link.getAttribute('data-page'));
-                const startIndex = (page - 1) * itemsPerPage;
-                const endIndex = startIndex + itemsPerPage;
-                const pageLinks = links.slice(startIndex, endIndex);
-                const toc = `<ul class="list-group">${pageLinks.join('')}</ul>`;
-                document.getElementById('toc').innerHTML = toc;
-                document.querySelectorAll('.page-item').forEach(item => item.classList.remove('active'));
-                link.parentElement.classList.add('active');
-            });
-        });
-        document.querySelector('.page-link[data-page="1"]').click();
-    })
-    .catch(error => console.error(error));
+let numPages = 0;
+const elementsPerPage = 6;
+let currentPage = 0;
+
+let pages = [];
+
+const fetchPages = async () => {
+    const response = await fetch(fetchURL);
+    const data = await response.json();
+    const pagesFiltered = data.filter(item => !(item.name.endsWith(".html") || item.name.endsWith(".js") || item.name.endsWith(".css")));
+    pages = pagesFiltered.map(page => page.name);
+    console.log(pages);
+}
+
+// Function to render the current page
+const renderPage = () => {
+    const start = currentPage * elementsPerPage;
+    const end = start + elementsPerPage;
+    const page = pages.slice(start, end);
+    console.log(page);
+    coursesList.innerHTML = '';
+    
+    page.forEach(course => {
+        const courseItem = document.createElement('div');
+        courseItem.classList.add('col');
+        courseItem.classList.add('card');
+        courseItem.innerHTML = `
+        <div class="card-body">
+        <div class="card-title roboto">
+            <b>
+                <h5>${course[0].toUpperCase()}${course.substring(1).replace("-", " ")}</h5>
+            </b>
+        </div>
+        <div class="card-text roboto">
+            <a href="${BASE_URL}/${course}" target="_blank">Ver clase</a>
+        </div>
+        </div>
+        `;
+        coursesList.appendChild(courseItem);
+    });
+}
+
+// Function to render the pagination using previous and next but first checking the current page at session storage
+const renderPagination = () => {
+    pagination.innerHTML = '';
+    if (currentPage > 0) {
+        const prevLi = document.createElement('li');
+        prevLi.classList.add('page-item');
+        const prevLink = document.createElement('a');
+        prevLink.classList.add('page-link');
+        prevLink.href = '#';
+        prevLink.textContent = 'Siguiente';
+        prevLink.addEventListener('click', () => {
+            currentPage--;
+            renderPage();
+            renderPagination();
+            setCurrentPage();
+        }
+        );
+        prevLi.appendChild(prevLink);
+        pagination.appendChild(prevLi);
+
+    }
+    if (currentPage < numPages - 1) {
+        const nextLi = document.createElement('li');
+        nextLi.classList.add('page-item');
+        const nextLink = document.createElement('a');
+        nextLink.classList.add('page-link');
+        nextLink.href = '#';
+        nextLink.textContent = 'Anterior';
+        nextLink.addEventListener('click', () => {
+            currentPage++;
+            renderPage();
+            renderPagination();
+            setCurrentPage();
+        }
+        );
+        nextLi.appendChild(nextLink);
+        pagination.appendChild(nextLi);
+    }
+}
+
+const checkCurrentPage = () => {
+    const currentPage = sessionStorage.getItem('currentPage');
+    if (currentPage) {
+        return parseInt(currentPage);
+    }
+    return 0;
+}
+
+const setCurrentPage = () => {
+    sessionStorage.setItem('currentPage', currentPage);
+}
+
+const main = async () => {
+    await fetchPages();
+    numPages = Math.ceil(pages.length / elementsPerPage);
+    currentPage = checkCurrentPage();
+    renderPage();
+    renderPagination();
+}
+
+main();
